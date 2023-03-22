bff3e4d5c4a7c7b42cfe5f9f23af4dc244e36b50
DSKIM <ehdtn7196@gmail.com>
2023-03-22 18:33:54 +0900

    11

---
+++ hello_world/10926.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#define MAX_ID 50
+using namespace std;
+
+int main()
+{
+	char ID[MAX_ID];
+
+	cin >> ID;
+
+	cout << ID << "??!" << endl;
+
+	return 0;
+}

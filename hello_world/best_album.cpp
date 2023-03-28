c6a4cb31ab77e92a31684671b1723de9e11ef09f
DSKIM <ehdtn7196@gmail.com>
2023-03-28 16:24:51 +0900

    0328

--- hello_world/best_album.cpp
+++ hello_world/best_album.cpp
@@ -1,25 +1,48 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <sstream>
+#include <map>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> solution(vector<string> genres, vector<int> plays) {
+vector<int> solution(vector<string> genres, vector<int> plays)
+{
     vector<int> answer;
 
-    for (int i = 0;i < genres.size();i++) {
+    string input;
+    getline(cin, input);
+    stringstream ss(input);
+    string genre;
 
-        cout << genres[i] << " ";
-
+    while (getline(ss, genre, ' '))
+    {
+        genres.push_back(genre);
     }
 
-    cout << endl;
-
-    for (int i = 0;i < plays.size();i++) {
-
-        cout << plays[i] << " ";
-
+    int n = genres.size();
+    vector<int> plays(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> plays[i];
     }
+
+    map<string, int> play_sum;
+    for (int i = 0; i < n; i++)
+    {
+        play_sum[genres[i]] += plays[i];
+    }
+
+    vector<pair<string, int>> genre_play_sum(play_sum.begin(), play_sum.end());
+    sort(genre_play_sum.begin(), genre_play_sum.end(), [](const auto& lhs, const auto& rhs)
+        {return lhs.second > rhs.second;});
+
+    string first_genre = genre_play_sum[0].first;
+    string second_genre = genre_play_sum[1].first;
+
+
+
 
     return answer;
 }
---
+++ hello_world/genreplay.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+vector<int> solution(vector<string> genres, vector<int> plays);
+{
+	vector<string> genres_input;
+	return answer
+}
